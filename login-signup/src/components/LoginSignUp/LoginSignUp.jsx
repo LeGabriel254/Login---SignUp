922e8076ed9f831d1f40bfe17c8b7b6c1d8d943b
Leon Gabriel <g73346920@gmail.com>
2024-09-02 23:40:08 +0300

    import images from assets

--- login-signup/src/components/LoginSignUp/LoginSignUp.jsx
+++ login-signup/src/components/LoginSignUp/LoginSignUp.jsx
@@ -1,9 +1,31 @@
 import React from "react";
-import '/.LoginSignUp.css'
+import './LoginSignUp.css'
 
+import person_icon from '../assets/name.png'
+import password_icon from '../assets/password.png'
+import email_icon from  '../assets/email.png'
 const LoginSignUp = () =>{
   return(
-    <></>
+  <div className="container">
+    <div className="header">
+      <div className="tex">Sign Up</div>
+      <div className="underline"></div>
+    </div>
+    <div className="inputs">
+      <div className="input">
+         <img src="" alt="" />
+         <input type="" />
+      </div>
+      <div className="input">
+         <img src="" alt="" />
+         <input type="" />
+      </div>
+      <div className="input">
+         <img src="" alt="" />
+         <input type="" />
+      </div>
+    </div>
+  </div>
   )
 };
 
